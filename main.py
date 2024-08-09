4b69c42a6cbf6ee31cb604707381fcca037fc1d5
Michaek Gonyk <maximqwerbich@gmail.com>
2024-08-09 12:57:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+print('Hello, World!')
+
+print(2 + 2 * 2)
+# цілі числа
+print(-10)
+print(0)
+print(5)
+
+# дійсні числа
+print(2.5)
+print(0.0000007)
+print(1.05)
+
+
+# множення - *, ділення - /, додавання - +, віднімання - -
+print(10 * 2)
+print(5 / 2)
+print(3 + 7.65)
+
+
+
+
+
+print(2-5)
+
+print(10 + 25 + 0)
+
+print((100 + 50 + 1000) / 3)
+
+
+print(19800 * 4)
+
+print(1200 * 8)
+
+
+# % - залишок від ділення
+print(125213 % 4652)
+
+
+# ЗМІННА
+name = 'John'
+age = 25
+firstName = 'Jane'
+SecondName = 'Doe'
+middle_name = 'sadad'
+
+# 123 = 'dfdsf' - не можна &&= 12
+
+_person = 'male'
+print(_person)
+
+
+num1 = 5
+num2 = 10
+
+print(num1 + num2)
+
+
+car = 'Honda'
+print('до вас їде ', car)
+
+c = 10
+d = 'Rocky'
+print(c + d)
+
